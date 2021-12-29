47467ea463f8b84bd4d10106c62b714759999f4b
artshenberg <76771299+artshenberg@users.noreply.github.com>
2021-12-29 11:05:41 +0800

    Initial commit

--- init.sh
+++ init.sh
@@ -1,11 +1,3 @@
-#!/bin/bash
-
-addr=$(pwd)
-files=(/etc/nginx.conf)
-# shellcheck disable=SC2068
-for f in ${files[@]}; do
-        sed -ri "s!\S*/web!${addr}!g" "${addr}""${f}"
-done
-
-sudo ln -sf "${addr}"/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/nginx.conf
+sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
